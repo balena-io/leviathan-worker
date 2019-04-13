21cb3c98173a7478c752f5eec38a666dac5bba53
Theodor Gherzan <theodor@resin.io>
2019-04-13 14:48:26 +0100

    Provide unit tests for our network feature
    Signed-off-by: Theodor Gherzan <theodor@resin.io>

--- test/api.spec.ts
+++ test/api.spec.ts
@@ -2,7 +2,8 @@ import 'mocha';
 import * as _ from 'lodash';
 import * as chai from 'chai';
 import * as chaiAsPromised from 'chai-as-promised';
-import * as testBot from '../lib/workers/testbot';
+import * as TestBot from '../lib/workers/testbot';
+import * as NetworkManager from '../lib/nm';
 import setup from '../lib/index';
 import { ImportMock, MockManager } from 'ts-mock-imports';
 
@@ -14,14 +15,33 @@ chai.use(chaiAsPromised);
 const { expect } = chai;
 
 describe('API', async () => {
-	const mockManager: MockManager<testBot.TestBot> = ImportMock.mockClass<
-		testBot.TestBot
-	>(testBot, 'default');
-	const app: Express.Application = await setup('/path/to/fake/worker');
+	let mockWorker: {
+		testbot: MockManager<TestBot.default>;
+		network: MockManager<NetworkManager.default>;
+	};
+	let app: Express.Application;
+
+	beforeEach(async () => {
+		mockWorker = {
+			testbot: ImportMock.mockClass<TestBot.default>(TestBot, 'default'),
+			network: ImportMock.mockClass<NetworkManager.default>(
+				NetworkManager,
+				'default',
+			),
+		};
+		app = await setup({
+			testbot: { devicePath: '/fake' },
+			network: {
+				apWifiIface: 'wlan0',
+				apWiredIface: 'eth0',
+			},
+		});
+	});
+
 	const errTest = new Error('TEST ERROR');
 
 	it('call /dut/on should turn testbot ON', async () => {
-		const spy = mockManager.mock('powerOn');
+		const spy = mockWorker.testbot.mock('powerOn');
 
 		const res = await chai.request(app).post('/dut/on');
 
@@ -31,7 +51,7 @@ describe('API', async () => {
 	});
 
 	it('call /dut/on should handle errors correctly', async () => {
-		const spy = mockManager.mock('powerOn').rejects(errTest);
+		const spy = mockWorker.testbot.mock('powerOn').rejects(errTest);
 
 		const res = await chai.request(app).post('/dut/on');
 
@@ -41,7 +61,7 @@ describe('API', async () => {
 	});
 
 	it('call /dut/off should turn testbot off', async () => {
-		const spy = mockManager.mock('powerOff');
+		const spy = mockWorker.testbot.mock('powerOff');
 
 		const res = await chai.request(app).post('/dut/off');
 
@@ -51,7 +71,7 @@ describe('API', async () => {
 	});
 
 	it('call /dut/off should handle errors correctly', async () => {
-		const spy = mockManager.mock('powerOff').rejects(errTest);
+		const spy = mockWorker.testbot.mock('powerOff').rejects(errTest);
 
 		const res = await chai.request(app).post('/dut/off');
 
@@ -61,7 +81,7 @@ describe('API', async () => {
 	});
 
 	it('call /dut/flash should turn testbot flash', async () => {
-		const spy = mockManager.mock('flash');
+		const spy = mockWorker.testbot.mock('flash');
 
 		const res = await chai.request(app).post('/dut/flash');
 
@@ -69,7 +89,65 @@ describe('API', async () => {
 		expect(spy.callCount).to.be.equal(1);
 	});
 
+	it('call /dut/network should correctly create wired and wireless connections', async () => {
+		const spies = [
+			mockWorker.network.mock('addWiredConnection'),
+			mockWorker.network.mock('addWirelessConnection'),
+		];
+
+		const res = await chai
+			.request(app)
+			.post('/dut/network')
+			.send({
+				wired: { nat: true },
+				wireless: { ssid: 'test', psk: 'secret' },
+			});
+
+		expect(res).to.have.status(200);
+		spies.forEach(spy => {
+			expect(spy.callCount).to.be.equal(1);
+		});
+	});
+
+	it('call /dut/network should correctly remove wired and wireless connections', async () => {
+		const spies = [
+			mockWorker.network.mock('removeWiredConnection'),
+			mockWorker.network.mock('removeWirelessConnection'),
+		];
+
+		const res = await chai
+			.request(app)
+			.post('/dut/network')
+			.send({});
+
+		expect(res).to.have.status(200);
+		spies.forEach(spy => {
+			expect(spy.callCount).to.be.equal(1);
+		});
+	});
+
+	it('call /dut/network should throw error if wireless configuration is incomplete', async () => {
+		const spies = [
+			mockWorker.network.mock('addWiredConnection'),
+			mockWorker.network.mock('addWirelessConnection'),
+			mockWorker.network.mock('removeWiredConnection'),
+			mockWorker.network.mock('removeWirelessConnection'),
+		];
+
+		const res = await chai
+			.request(app)
+			.post('/dut/network')
+			.send({ wired: { nat: true }, wireless: { ssid: 'test' } });
+
+		expect(res.text).to.be.equal('Wireless configuration incomplete');
+		expect(res).to.have.status(500);
+		spies.forEach(spy => {
+			expect(spy.callCount).to.be.equal(0);
+		});
+	});
+
 	afterEach(() => {
-		mockManager.restore();
+		mockWorker.testbot.restore();
+		mockWorker.network.restore();
 	});
 });
